c4d256eb77a0ef7b3d3bfabc48a44678ed3ea831
AzazelloB <43464264+AzazelloB@users.noreply.github.com>
2018-10-01 18:41:14 +0300

    Added Pause Menu
    Pause Menu prefab doesn't work, googled this issue and it exist couple years and doesn't solved yet. So I dunno

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isPaused = false;
+
+    public GameObject pauseMenu;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void MainMenu(string level)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(level);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
